2baf41c89e11e56ea24878b7bb3521c28b937dd5
lcw1030 <68216490+lcw1030@users.noreply.github.com>
2021-06-21 00:06:49 +0900

    Update distance.ino

--- arduino/distance.ino
+++ arduino/distance.ino
@@ -26,20 +26,6 @@ void setup() {
   bluetooth.begin(9600); // 통신 속도 9600bps로 블루투스 시리얼 통신 시작
 }
 
-int check_pir() {
-  int human = digitalRead(PIR);
-  if(human == HIGH) {
-    digitalWrite(LEDY, HIGH);
-    Serial.println("PIR: ON");
-    //s.println("PIR: ON");
-  } else {
-    digitalWrite(LEDY, LOW);
-    Serial.println("PIR: OFF");
-    //s.println("PIR:OFF");
-  }
-  return human;
-}
-
 double check_distance(){
   double distance, duration;
   /* Ultrasonic Sensor */
@@ -50,44 +36,19 @@ double check_distance(){
   digitalWrite(trigPin,LOW);
   duration = pulseIn(echoPin,HIGH);
   distance = duration / 29 / 2;
-  Serial.print("Distance2: ");
+  Serial.print("Distance: ");
   Serial.println(distance);
-  //bluetooth.print("Distance2: ");
-  //bluetooth.println(distance);
   return distance;
 }
 
-void check_status(int human, double distance) {
-  //Serial.print("human: ");
-  //Serial.println(human);
-  if (distance >= 3000) {
-    Serial.print("ddd: ");
-    Serial.println(distance);
-    return;
-  }
-  
-  if (distance <= 70){
-    //Serial.println("1");
-    digitalWrite(LEDG, HIGH);
-    digitalWrite(LEDR, LOW);
-    //s.println("Studying");
-  } else {
-    //Serial.println("0");
-    digitalWrite(LEDR, HIGH);
-    digitalWrite(LEDG, LOW);
-    //s.println("Not Studying");
-  }
-}
-
 void loop() {
-  //int human = check_pir();
   double distance = check_distance();
   char distance_str[10];
   dtostrf(distance,4, 2, distance_str); 
   char str[128];
-  sprintf(str, "{data:[%s], type:0}", distance_str);
+  int n = sprintf(str, "{data:[%s], type:0}", distance_str);
+  //str[n] = '\0';
   Serial.println(str);
   bluetooth.println(str);
-  //check_status(human, distance);
   delay(period);
 }
